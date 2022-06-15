ea547176f939d0f1a220e1f0ba2b65a27f66598a
Anirudh <102802211+aspiringDev22@users.noreply.github.com>
2022-06-15 17:35:15 +0530

    Update script.js

--- video trailer popup/script.js
+++ video trailer popup/script.js
@@ -1,6 +1,7 @@
 const btnEl = document.querySelector(".btn");
 const trailerContainerEl = document.querySelector(".trailer-container");
 const closeIconEl = document.querySelector(".close-icon");
+const videoEL = document.querySelector("video");
 
 btnEl.addEventListener("click", () => {
   trailerContainerEl.classList.remove("active");
@@ -8,4 +9,6 @@ btnEl.addEventListener("click", () => {
 
 closeIconEl.addEventListener("click", () => {
   trailerContainerEl.classList.add("active");
+videoEL.pause();
+  videoEl.currentTime = 0;
 });
